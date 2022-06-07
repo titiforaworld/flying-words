21c865ca96182acfc6bbc644f02c3e5d2f14ee84
Celine Riou <celineriou@gmail.com>
2022-06-07 18:07:21 +0200

    start main package

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
-from flying_words.audio import Audio
+from flying_words.audio import Audio, merge_diffusion_with_samples
 from flying_words.diarization import Diarization
 from flying_words.google_clients import BigQueryClient, StorageClient
 from flying_words.transcription import Transcription
+from flying_words.speaker import Speaker
 
 import pandas as pd
 
@@ -29,8 +30,10 @@ project_name = 'intense-elysium-346915'
 bucket_name = 'le-wagon-project-75667-antoine'
 credential_path = '/home/clement/code/titiforaworld/gcp/intense-elysium-346915-f2127c89e62b.json'
 
-# # Instanciate google client
-# gsClient = StorageClient(project_name, credential_path)
+
+# # Instanciate google clients
+gsClient = StorageClient(project = project_name, credentials = credential_path)
+big_query = BigQueryClient(project = project_name, credentials = credential_path)
 
 # # Get blobs from bucket_name
 # blobs = gsClient.get_bucket_blobs(bucket_name)
@@ -44,13 +47,38 @@ credential_path = '/home/clement/code/titiforaworld/gcp/intense-elysium-346915-f
 # input_path = '/home/clement/code/titiforaworld/flying_words/raw_data/001_test'
 # gsClient.upload_blob(input_path, bucket_name, 'data/')
 
+# get episod & samples from BQ
+job = big_query.get_table(dataset='flying_words', table_name='view_target_output')
+
+# get merged audio file
+merge_audio_info = merge_diffusion_with_samples(job,gsClient)
+audio_wav = merge_audio_info['merged_audio']
+
 # Diarization
-# test_audio = Audio('/home/clement/code/titiforaworld/flying_words/raw_data/samples/16119-17.04.2022-ITEMA_22998007-2022C6119S0107-21_sample_0_600_unknown.wav')
-# diarization = Diarization(source=test_audio)
-# diarization.make_diarization()
-# print(diarization.diarization_output())
+diarization_audiowav = Diarization(audio_wav)
+diarization_audiowav.make_diarization(min_duration_off = 1.0)
+segmentation_df = diarization_audiowav.get_diarization_df()
 
 # test_audio = Audio('/home/clement/code/titiforaworld/flying_words/raw_data/16119-17.04.2022-ITEMA_22998007-2022C6119S0107-21.mp3')
 # sample = test_audio.export_sample(20, 20)
+
+# upload speaker samples
+# get needed inputs
+known_ids = merge_audio_info['known_ids']
+unknown_id = merge_audio_info['unknown_id']
+episod_id = merge_audio_info['episode_id']
+start_ep = merge_audio_info['show_start']
+# get needed information for sample extract
+speaker = Speaker()
+ep_info = speaker.get_unknown_info(segmentation_df, known_ids, unknown_id)
+# get retreated dataframe
+retreated_df = speaker.retreated_dataframe(segmentation_df, episod_id, unknown_id, start_ep)
+
+# upload retreated dataframe to Big Query
+big_query.append_row_to_table(dataset='flying_words',  input_df = retreated_df, dest_table='segmentation')
+
+# upload speaker samples to Could Storage
+sample_dataset = "personnality_sample"
+speaker.upload_samples_tables(audio_file=merge_audio_info['show_start'], gsClient=gsClient, big_query=big_query, bucket_name=bucket_name, sample_dataset=sample_dataset)
 
 # transcription = Transcription(sample).make_transcription()
